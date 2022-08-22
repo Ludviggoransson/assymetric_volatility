3b9a277ffbf3a88412802133d4fbe0bc4b541fb5
Ludviggoransson <48404469+Ludviggoransson@users.noreply.github.com>
2022-08-22 12:32:42 +0200

    Update pdf_decomposed_standard_laplace.py

--- src/pdf_decomposed_standard_laplace.py
+++ src/pdf_decomposed_standard_laplace.py
@@ -7,7 +7,7 @@ from volatility_process import VolatilityProcess
 
 class DecomposedSLC(VolatilityProcess):
 
-    def assymmetric_pdf(self, rho:np.array, theta:float=0.5):
+    def asymmetric_pdf(self, rho:np.array, theta:float=0.5):
 
         rho_t = rho[1:]
         rho_lag = rho[0:-1]
@@ -47,7 +47,7 @@ if __name__ == "__main__":
     alphas=np.array([0.1, 0.2, 0.3, 0.4])
     n = 100
     rho = np.linspace(1.4255,10,80000)
-    pdf = [DecomposedSLC(alpha=alpha).symmetric_pdf(rho=rho) for alpha in alphas]
+    pdf = [DecomposedSLC(alpha=alpha).asymmetric_pdf(rho=rho) for alpha in alphas]
     df_pdf = pd.DataFrame(np.transpose(pdf))
     df_pdf.columns =['alpha='+str(alphas[0]), 'alpha='+str(alphas[1]), 'alpha='+str(alphas[2]), 'alpha='+str(alphas[3])]
     sns.lineplot(data=df_pdf).set(title='PDF varying alpha')
